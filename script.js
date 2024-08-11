04fad0e26a22ca44908c1b0d0d2a36d33ca39abe
trodlop <161824279+trodlop@users.noreply.github.com>
2024-08-11 14:49:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const decibels = document.getElementById("decibels");
+const refresh = document.getElementById("refresh");
 
 decibels.innerText = "testing123"
 
 var data = [5,7,3,98,14,5]
 
 decibels.innerText += data
+
